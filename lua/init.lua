7842d93bc2339e224c81031c4ad010757014e9a0
dan <terakael@gmail.com>
2025-07-25 21:42:06 +0900

    removed gpt stuff in favour of claude code

--- lua/init.lua
+++ lua/init.lua
@@ -209,89 +209,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-
-local function keymapOptions(desc)
-  return {
-    noremap = true,
-    silent = true,
-    nowait = true,
-    desc = 'GPT prompt ' .. desc,
-  }
-end
-
--- Chat commands
-
-vim.keymap.set({ 'n', 'i' }, '<C-g>c', '<cmd>GpChatNew<cr>', keymapOptions 'New Chat')
-vim.keymap.set({ 'n', 'i' }, '<C-g>t', '<cmd>GpChatToggle<cr>', keymapOptions 'Toggle Chat')
-
-vim.keymap.set({ 'n', 'i' }, '<C-g>f', '<cmd>GpChatFinder<cr>', keymapOptions 'Chat Finder')
-
-vim.keymap.set('v', '<C-g>c', ":<C-u>'<,'>GpChatNew<cr>", keymapOptions 'Visual Chat New')
-vim.keymap.set('v', '<C-g>p', ":<C-u>'<,'>GpChatPaste<cr>", keymapOptions 'Visual Chat Paste')
-vim.keymap.set('v', '<C-g>t', ":<C-u>'<,'>GpChatToggle<cr>", keymapOptions 'Visual Toggle Chat')
-
-vim.keymap.set({ 'n', 'i' }, '<C-g><C-x>', '<cmd>GpChatNew split<cr>', keymapOptions 'New Chat split')
-vim.keymap.set({ 'n', 'i' }, '<C-g><C-v>', '<cmd>GpChatNew vsplit<cr>', keymapOptions 'New Chat vsplit')
-vim.keymap.set({ 'n', 'i' }, '<C-g><C-t>', '<cmd>GpChatNew tabnew<cr>', keymapOptions 'New Chat tabnew')
-
-vim.keymap.set('v', '<C-g><C-x>', ":<C-u>'<,'>GpChatNew split<cr>", keymapOptions 'Visual Chat New split')
-vim.keymap.set('v', '<C-g><C-v>', ":<C-u>'<,'>GpChatNew vsplit<cr>", keymapOptions 'Visual Chat New vsplit')
-vim.keymap.set('v', '<C-g><C-t>', ":<C-u>'<,'>GpChatNew tabnew<cr>", keymapOptions 'Visual Chat New tabnew')
-
--- Prompt commands
-vim.keymap.set({ 'n', 'i' }, '<C-g>r', '<cmd>GpRewrite<cr>', keymapOptions 'Inline Rewrite')
-vim.keymap.set({ 'n', 'i' }, '<C-g>a', '<cmd>GpAppend<cr>', keymapOptions 'Append (after)')
-vim.keymap.set({ 'n', 'i' }, '<C-g>b', '<cmd>GpPrepend<cr>', keymapOptions 'Prepend (before)')
-
-vim.keymap.set('v', '<C-g>r', ":<C-u>'<,'>GpRewrite<cr>", keymapOptions 'Visual Rewrite')
-vim.keymap.set('v', '<C-g>a', ":<C-u>'<,'>GpAppend<cr>", keymapOptions 'Visual Append (after)')
-vim.keymap.set('v', '<C-g>b', ":<C-u>'<,'>GpPrepend<cr>", keymapOptions 'Visual Prepend (before)')
-vim.keymap.set('v', '<C-g>i', ":<C-u>'<,'>GpImplement<cr>", keymapOptions 'Implement selection')
-
-vim.keymap.set({ 'n', 'i' }, '<C-g>gp', '<cmd>GpPopup<cr>', keymapOptions 'Popup')
-vim.keymap.set({ 'n', 'i' }, '<C-g>ge', '<cmd>GpEnew<cr>', keymapOptions 'GpEnew')
-vim.keymap.set({ 'n', 'i' }, '<C-g>gn', '<cmd>GpNew<cr>', keymapOptions 'GpNew')
-vim.keymap.set({ 'n', 'i' }, '<C-g>gv', '<cmd>GpVnew<cr>', keymapOptions 'GpVnew')
-vim.keymap.set({ 'n', 'i' }, '<C-g>gt', '<cmd>GpTabnew<cr>', keymapOptions 'GpTabnew')
-
-vim.keymap.set('v', '<C-g>gp', ":<C-u>'<,'>GpPopup<cr>", keymapOptions 'Visual Popup')
-vim.keymap.set('v', '<C-g>ge', ":<C-u>'<,'>GpEnew<cr>", keymapOptions 'Visual GpEnew')
-vim.keymap.set('v', '<C-g>gn', ":<C-u>'<,'>GpNew<cr>", keymapOptions 'Visual GpNew')
-
-vim.keymap.set('v', '<C-g>gv', ":<C-u>'<,'>GpVnew<cr>", keymapOptions 'Visual GpVnew')
-vim.keymap.set('v', '<C-g>gt', ":<C-u>'<,'>GpTabnew<cr>", keymapOptions 'Visual GpTabnew')
-
-vim.keymap.set({ 'n', 'i' }, '<C-g>x', '<cmd>GpContext<cr>', keymapOptions 'Toggle Context')
-vim.keymap.set('v', '<C-g>x', ":<C-u>'<,'>GpContext<cr>", keymapOptions 'Visual Toggle Context')
-
-vim.keymap.set({ 'n', 'i', 'v', 'x' }, '<C-g>s', '<cmd>GpStop<cr>', keymapOptions 'Stop')
-vim.keymap.set({ 'n', 'i', 'v', 'x' }, '<C-g>n', '<cmd>GpNextAgent<cr>', keymapOptions 'Next Agent')
-
--- optional Whisper commands with prefix <C-g>w
-vim.keymap.set({ 'n', 'i' }, '<C-g>ww', '<cmd>GpWhisper<cr>', keymapOptions 'Whisper')
-vim.keymap.set('v', '<C-g>ww', ":<C-u>'<,'>GpWhisper<cr>", keymapOptions 'Visual Whisper')
-
-vim.keymap.set({ 'n', 'i' }, '<C-g>wr', '<cmd>GpWhisperRewrite<cr>', keymapOptions 'Whisper Inline Rewrite')
-vim.keymap.set({ 'n', 'i' }, '<C-g>wa', '<cmd>GpWhisperAppend<cr>', keymapOptions 'Whisper Append (after)')
-vim.keymap.set({ 'n', 'i' }, '<C-g>wb', '<cmd>GpWhisperPrepend<cr>', keymapOptions 'Whisper Prepend (before) ')
-
-vim.keymap.set('v', '<C-g>wr', ":<C-u>'<,'>GpWhisperRewrite<cr>", keymapOptions 'Visual Whisper Rewrite')
-vim.keymap.set('v', '<C-g>wa', ":<C-u>'<,'>GpWhisperAppend<cr>", keymapOptions 'Visual Whisper Append (after)')
-vim.keymap.set('v', '<C-g>wb', ":<C-u>'<,'>GpWhisperPrepend<cr>", keymapOptions 'Visual Whisper Prepend (before)')
-
-vim.keymap.set({ 'n', 'i' }, '<C-g>wp', '<cmd>GpWhisperPopup<cr>', keymapOptions 'Whisper Popup')
-vim.keymap.set({ 'n', 'i' }, '<C-g>we', '<cmd>GpWhisperEnew<cr>', keymapOptions 'Whisper Enew')
-vim.keymap.set({ 'n', 'i' }, '<C-g>wn', '<cmd>GpWhisperNew<cr>', keymapOptions 'Whisper New')
-vim.keymap.set({ 'n', 'i' }, '<C-g>wv', '<cmd>GpWhisperVnew<cr>', keymapOptions 'Whisper Vnew')
-vim.keymap.set({ 'n', 'i' }, '<C-g>wt', '<cmd>GpWhisperTabnew<cr>', keymapOptions 'Whisper Tabnew')
-
-vim.keymap.set('v', '<C-g>wp', ":<C-u>'<,'>GpWhisperPopup<cr>", keymapOptions 'Visual Whisper Popup')
-vim.keymap.set('v', '<C-g>we', ":<C-u>'<,'>GpWhisperEnew<cr>", keymapOptions 'Visual Whisper Enew')
-
-vim.keymap.set('v', '<C-g>wn', ":<C-u>'<,'>GpWhisperNew<cr>", keymapOptions 'Visual Whisper New')
-vim.keymap.set('v', '<C-g>wv', ":<C-u>'<,'>GpWhisperVnew<cr>", keymapOptions 'Visual Whisper Vnew')
-vim.keymap.set('v', '<C-g>wt', ":<C-u>'<,'>GpWhisperTabnew<cr>", keymapOptions 'Visual Whisper Tabnew')
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -478,6 +395,14 @@ require('lazy').setup({
         --   },
         -- },
         -- pickers = {}
+        defaults = {
+          file_ignore_patterns = {
+            '.venv',
+            '__pycache__',
+            '.mypy_cache',
+            '.git',
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -503,7 +428,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>gs', function()
-        builtin.git_status({ git_icons = { changed = "M", added = "A", deleted = "D", renamed = "R", untracked = "?" } })
+        builtin.git_status { git_icons = { changed = 'M', added = 'A', deleted = 'D', renamed = 'R', untracked = '?' } }
       end, { desc = '[G]it [S]tatus' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
@@ -1018,21 +943,6 @@ require('lazy').setup({
     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  },
-  {
-    'robitx/gp.nvim',
-    config = function()
-      local conf = {
-        providers = {
-          openai = {
-            disable = false,
-            endpoint = 'https://fdap-ai-poc.openai.azure.com/openai/deployments/gpt-4o-20240513/chat/completions?api-version=2024-02-01',
-            secret = os.getenv 'AZURE_OPENAI_API_KEY',
-          },
-        },
-      }
-      require('gp').setup(conf)
-    end,
   },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
